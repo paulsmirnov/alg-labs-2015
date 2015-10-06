92fb315258664899ec0897eaec5dd5a11c843b66
Paul Smirnov <paul.o.smirnov@gmail.com>
2015-10-06 18:31:17 +0300

    ??????? ??????? XPR, STR, BIT

--- alg-labs-2015.bat
+++ alg-labs-2015.bat
@@ -1,4 +1,10 @@
 pdflatex -shell-escape --aux-directory=.\_aux %~n0.tex
+if errorlevel 1 goto end
 bibtex8 -B -c utf8cyrillic.csf .\_aux\%~n0
+if errorlevel 1 goto end
 pdflatex -shell-escape --aux-directory=.\_aux %~n0.tex
+if errorlevel 1 goto end
 pdflatex -shell-escape --aux-directory=.\_aux %~n0.tex
+if errorlevel 1 goto end
+
+:end
