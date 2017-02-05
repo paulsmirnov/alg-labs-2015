c283b97f8d54d7272ffc33bd0bfe7e34b0935bb9
Paul Smirnov <s.paul@mail.ru>
2017-02-05 09:41:19 +0300

    ??????? ?????? ???????? ??-?? ??????? ? ??????? ????????

--- alg-labs-2015.bat
+++ alg-labs-2015.bat
@@ -1,3 +1,6 @@
+pdflatex -shell-escape --aux-directory=.\_aux %~n0-cover.tex
+if errorlevel 1 goto end
+
 pdflatex -shell-escape --aux-directory=.\_aux %~n0.tex
 if errorlevel 1 goto end
 bibtex8 -B -c utf8cyrillic.csf .\_aux\%~n0
