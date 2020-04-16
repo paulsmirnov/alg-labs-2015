ba52ebcf1239d96ef774750d5a88f54169ede827
Paul Smirnov <s.paul@mail.ru>
2020-04-16 09:31:31 +0300

    Merge cover

--- alg-labs-2015.bat
+++ alg-labs-2015.bat
@@ -9,6 +9,8 @@ pdflatex -shell-escape --aux-directory=.\_aux %~n0.tex
 if errorlevel 1 goto end
 pdflatex -shell-escape --aux-directory=.\_aux %~n0.tex
 if errorlevel 1 goto end
+rem pdftk.exe A=alg-labs-2015-cover.pdf B=alg-labs-2015.pdf cat A1-2 B A3 output alg-labs-2015-full.pdf
+if errorlevel 1 goto end
 start alg-labs-2015.pdf
 
 :end
